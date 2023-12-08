ad145dbd908416815d792ba334b6dbce2810c0ce
LahiruHarshana <lharshana2002@gmail.com>
2023-12-08 10:01:32 +0530

    updated

--- src/main/java/lk/ijse/gdse66/hello/api/servletlifecycle/HelloServlet.java
+++ src/main/java/lk/ijse/gdse66/hello/api/servletlifecycle/HelloServlet.java
@@ -14,25 +14,22 @@ public class HelloServlet extends HttpServlet {
         System.out.println("Servlet()");
     }
 
+    @Override
     public void init(ServletConfig config) throws ServletException {
         System.out.println("init(ServletConfig)");
-        super.init(config);
     }
 
+    @Override
     public void init() throws ServletException {
         System.out.println("init()");
-        super.init();
     }
-
-
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         System.out.println("do Get");
     }
 
     @Override
-    void destroy(){
+    public void destroy(){
         System.out.println("HelloServlet object is destroyed");
-        super.destroy();
     }
 }
